5a5d6e4cae9948b2fc931b229767cb55986728df
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-10-09 09:59:06 +0200

    Greet mars

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello mars!");
         System.out.println("Hello everybody!");
     }
 }
